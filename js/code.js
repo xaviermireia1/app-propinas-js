01d7ade1254bcc5e150008224f90f1c981d23182
Xavi <100006207.joan23@fje.edu>
2021-10-08 19:51:28 +0200

    second commit

--- js/code.js
+++ js/code.js
@@ -1,3 +1,12 @@
 function calculo() {
-    alert("hellow");
+    //alert("hellow");
+    let totalCuenta = document.getElementById('total').value;
+    //alert(totalCuenta);
+    // let servicio = document.getElementsByName('servicio')[0].value;
+    let servicio = document.getElementById('servicio').value;
+    //alert(servicio);
+    // let personas = document.getElementsByClassName('personas')[0].value;
+    let personas = document.getElementById('personas').value;
+    //alert(personas);
+    console.log('Total: ' + totalCuenta + 'servicio: ' + servicio + 'Personas: ' + personas);
 }
