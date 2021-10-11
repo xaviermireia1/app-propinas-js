00bff850ef4c97ca6b258ad56d79c13988880a05
Xavi <100006207.joan23@fje.edu>
2021-10-11 19:39:21 +0200

    calculadora finalizada con JS

--- js/code.js
+++ js/code.js
@@ -8,5 +8,19 @@ function calculo() {
     // let personas = document.getElementsByClassName('personas')[0].value;
     let personas = document.getElementById('personas').value;
     //alert(personas);
-    console.log('Total: ' + totalCuenta + 'servicio: ' + servicio + 'Personas: ' + personas);
+    let resultado = (servicio * totalCuenta) / personas;
+    console.log('Total: ' + resultado);
+    if (isNaN(resultado)) {
+        alert('No se ha introducido ningun dato');
+    } else if (personas == '' || personas == 0) {
+        alert('No se ha introducido ningun dato');
+    } else {
+        // innerHTML es para ver el contenido del javascript en el html (Donde llamo a la función)
+        document.getElementById("res").innerHTML = "<p>Total a pagar</p><h3>" + resultado + " por persona </h3>";
+    }
+}
+// Para limpiar los resultados del formulario más el resultado
+function limpiar() {
+    document.getElementById("form").reset();
+    document.getElementById("res").innerHTML = "";
 }
